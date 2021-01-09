fd9af2d5b5025f5fbe5f8fe62f1b78f12a979e25
siva venkata datta sanka <60128378+sivasanka1996@users.noreply.github.com>
2021-01-09 16:05:23 +0530

    user1

---
+++ User Management/User Management/user_1.js
@@ -0,0 +1,161 @@
+var user = {};
+var usersList = [];
+var userIdCount = 1;
+var firstName = document.forms['createform']['firstName']
+var lastName = document.forms['createform']['lastName']
+var email = document.forms['createform']['email']
+var password = document.forms['createform']['password']
+var ConfirmPassword = document.forms['createform']['ConfirmPassword']
+var favhero = document.forms['createform']['FavoriteHero']
+var age = document.forms['createform']['age']
+
+
+var firstName_error = document.getElementById('firstName_error');
+var lastName_error = document.getElementById('lastName_error');
+var email_error = document.getElementById('email_error');
+var password_error = document.getElementById('password_error');
+var ConfirmPassword_error = document.getElementById('ConfirmPassword_error');
+var age_error = document.getElementById('age_error');
+
+function validated()
+{
+if(firstName.value.length < 3){
+firstName.style.border = "1px solid red";
+firstName_error.style.display = "block";
+firstName.focus();
+   return false;
+}
+if(lastName.value.length < 3){
+lastName.style.border = "1px solid red";
+lastName_error.style.display = "block";
+lastName.focus();
+return false;
+}
+if(email.value.length < 9){
+    email.style.border = "1px solid red";
+    email_error.style.display = "block";
+    email.focus();
+    return false;
+    }
+if(password.value.length < 5){
+    password.style.border = "1px solid red";
+    password_error.style.display = "block";
+    password.focus();
+    return false;
+    }
+if(ConfirmPassword.value.length < 5){
+    ConfirmPassword.style.border = "1px solid red";
+    ConfirmPassword_error.style.display = "block";
+    ConfirmPassword.focus();
+    return false;
+    }
+if(age.value.length < 1){
+    age.style.border = "1px solid red";
+    age_error.style.display = "block";
+    age.focus();
+    return false;
+ }    
+if (usersList.length !== 0 && usersList.filter(x => x.email == $("#email").val()).length !== 0) {
+    window.alert("the email" + $("#email").val() + "already exist");
+  } 
+  else 
+  {
+    user = {
+        userId : userIdCount,
+        firstName: $("#firstName").val(),
+        lastName: $("#lastName").val(),
+        email: $("#email").val(),
+        password: $("#password").val(),
+        confirmpassword: $('#confirm_password').val(), 
+        age : $("#age").val(),
+        game: [],
+        isAdmin : $("#admin:checked").length === 1 ? true : false
+    }
+    usersList.push(user);
+    userIdCount++;
+    console.log(usersList);
+    return false;
+}
+//   console.log(userList);
+  return false;
+}
+
+var userIdCount = 1;
+function checkPass(){
+    var pass  = document.getElementById("password").value;
+    var rpass  = document.getElementById("confirm_password").value;
+   if(pass != rpass){
+       alert("Password not matching");
+    //    document.getElementById("submit").disabled = true;
+    //    $('.missmatch').html("Entered Password is not matching!! Try Again");
+   }
+//    else{
+//        $('.missmatch').html("");
+//        document.getElementById("submit").disabled = false;
+//    }
+}
+function createAccount(){
+    user = {
+        
+        userId : userIdCount,
+        firstName: $("#firstName").val(),
+        lastName: $("#lastName").val(),
+        email: $("#email").val(),
+        password: $("#password").val(),
+        confirmpassword: $('#confirm_password').val(), 
+        age : $("#age").val(),
+        game: [],
+        isAdmin : $("#admin:checked").length === 1 ? true : false
+    }
+    $(".game:checked").each(function(){
+        user.game.push($(this).val());
+    });
+    usersList.push(user);
+    userIdCount++;
+    console.log(usersList);
+}
+
+function login(){
+    var k;
+    var isUserLoggedIn = false;
+    var loggedInUserDetails;
+    if(usersList.length === 0){
+        alert("Enter user details");
+    } else {
+        for(k = 0; k < usersList.length; k++) {
+            if(usersList[k].email === $("#loginEmail").val() && usersList[k].password === $("#loginPassword").val()){
+                isUserLoggedIn = true;
+                loggedInUserDetails = usersList[k];
+            }
+        }
+        if(isUserLoggedIn === true){
+            $("#userListTable tbody").html("");
+            $(".pages").addClass('hide');
+            $(".homePage").removeClass('hide');
+   
+            window.location.href="home1.html";
+
+        } 
+        else {
+            alert("Please enter the valid Login details");
+        }
+    }
+}
+
+
+
+
+$(document).ready(function(){
+    $(".createAccountLink").click(function(){
+        $(".pages").addClass('hide');
+        $(".createAccountPage").removeClass("hide");
+        $(".nav li").removeClass("active");
+        $(this).addClass("active");                     
+    });
+    $(".loginLink").click(function(){
+        $(".pages").addClass('hide');
+        $(".loginPage").removeClass("hide");
+        $(".nav li").removeClass("active");
+        $(this).addClass("active");                     
+    });
+})             
